31b938c9493da5e660f2e37b40e77037bf74e57a
Matheus <dr_4_gao@hotmail.com>
2018-06-08 17:12:05 -0300

    7 desfecho adicionado

--- Diagnostico_Lombalgia/www/js/index.js
+++ Diagnostico_Lombalgia/www/js/index.js
@@ -216,7 +216,8 @@ var app = {
                                 $("#diagnostico").html("Lombalgia aguda com dor referida para membros inferiores");
                                    //Desfecho 7
                             } else if (localStorage.getItem('quintaEscolha') == 1){
-                                  $("#resultado").html("Desfecho 7"); 
+                                 $("#diagnostico").html("Lombalgia aguda associada a disturbios afetivos e/ou cogitivos");
+                                $("#tratamento").html("Educação e encaminhamento específico para o estado cognitivo apresentado pelo paciente, isto é, depressão, medo e catastrofização");
                             }
                     
                             }
